2cebc9d958588a2751f61c5838995e438988d4f9
lynn <49941232+m-lynn@users.noreply.github.com>
2019-06-01 14:51:41 +0300

    Update bash19.sh

--- lab_suzen/bash19.sh
+++ lab_suzen/bash19.sh
@@ -3,4 +3,3 @@ mkdir -P Documents/projects/lab19/using/simple/bash/commands
 cd Documents/projects/lab19/using/simple/bash/commands
 sleep 60
 ls
-echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
