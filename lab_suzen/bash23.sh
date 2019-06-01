91f01a50c68a1257452c8b90386d3432d4dbc933
lynn <49941232+m-lynn@users.noreply.github.com>
2019-06-01 12:10:38 +0300

    Update bash23.sh

--- lab_suzen/bash23.sh
+++ lab_suzen/bash23.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-mv nginx*.log nginx*.log.back
+cd destination
+mv nginx1.log nginx1.log.back
+mv nginx2.log nginx2.log.back
+mv nginx3.log nginx3.log.back
+mv nginx4.log nginx4.log.back
+mv nginx5.log nginx5.log.back
+cd ../source
 mv *.* /home/suzen/destination
 echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
